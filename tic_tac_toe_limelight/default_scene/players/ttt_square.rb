5e7c1e6d946f1748e12a5574c93402a5ac55367c
Adam Gooch <adamgooch@acm.org>
2012-07-31 10:33:16 -0400

    Added limelight GUI and quick play scripts

---
+++ tic_tac_toe_limelight/default_scene/players/ttt_square.rb
@@ -0,0 +1,54 @@
+require 'java'
+
+import 'gooch.tictactoe.Board'
+
+module TttSquare
+
+  X = 88
+  O = 79
+
+  def mouse_clicked (event)
+    if self.text == "" && !game_over
+      id = self.id
+      production.board.putMarkInSquare(X, id.to_i)
+      if !game_over 
+        production.ai.move(O)
+      end
+      drawboard
+      if game_over
+        show_message 
+      end
+    end
+  end
+
+  def drawboard
+    9.times do |i|
+      mark = production.board.getMarkInSquare(i)
+      if mark == X || mark == O 
+        scene.find(i).text = mark.chr
+      end
+    end
+  end
+
+  def game_over
+    production.analyzer.thereIsAWinner() || production.board.countSquaresAvailable() == 0 
+  end
+
+  def show_message
+    message = get_message
+    scene.build do
+      __install 'partials/message_label.rb', :text => message
+    end
+  end
+
+  def get_message
+    if production.analyzer.getWinner() == X  
+      return "I Can't Believe It!"
+    elsif production.analyzer.getWinner() == O
+      return "I Win!!"
+    else
+      return "Draw."
+    end
+  end
+
+end
---
+++ tic_tac_toe_limelight/stages.rb
@@ -0,0 +1,7 @@
+stage "default" do
+  default_scene "default_scene"
+  title "Tic Tac Toe"
+  location [200, 200]
+  size [500, 500]
+end
+
